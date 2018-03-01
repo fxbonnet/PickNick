51e773dd200ad4a60bb834f7e83e2c47c2c87e5d
Francois-Xavier Bonnet <fx.bonnet@gmail.com>
2018-03-01 13:06:17 +1100

    Scala version in dependencies and sbt version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,17 @@ import sbt._
 
 object Dependencies {
 
-  val specsVersion = "3.9.0"
+  val specsVersion = "4.0.3"
 
   val testDependencies = Seq(
-    "org.specs2" % "specs2-core_2.12" % specsVersion % "test",
-    "org.specs2" % "specs2-mock_2.12" % specsVersion % "test"
+    "org.specs2" %% "specs2-core" % specsVersion % Test,
+    "org.specs2" %% "specs2-mock" % specsVersion % Test,
+    "junit" % "junit" % "4.12" % Test,
+    "com.novocode" % "junit-interface" % "0.11" % Test
   )
 
-  lazy val coreDependencies: Seq[ModuleID] = testDependencies
+  val coreDependencies: Seq[ModuleID] = testDependencies ++ Seq(
+    "org.apache.commons" % "commons-lang3" % "3.7",
+    "commons-codec" % "commons-codec" % "1.11")
 
 }
